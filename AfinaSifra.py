86114d5a4d0cca213a31afcec36a2ea8a5897006
Ivan Peranovic <crimson6999@gmail.com>
2019-03-05 19:23:05 +0100

    edit

--- AfinaSifra.py
+++ AfinaSifra.py
@@ -3,17 +3,19 @@ ekripIliDekrip = int(input("Enkriptiranje(0) ili dekriptiranje(1)? "))
 if ekripIliDekrip is 0:
     rijec = input("Unesi rijec koju zelis enkriptirati: ").lower()
     zelimrazmak = int(input("Zelite li sakriti razmake, Da = 1, Ne = 0?: "))
-    kljuc = int(input("Unesite prvi broj kljuca: ")),int(input("Unesite drugi broj kljuca: "))
-    kriplistaslova = []
-    for x in rijec:
-        if x.isspace() and zelimrazmak == 1:
-            print("",end="")
-        elif x.isspace() and zelimrazmak == 0:
-            print(" ",end="")
-        elif x in abeceda:
-            print(abeceda[(kljuc[0]*int(abeceda.index(x))+kljuc[1]) % 26], end="")
-        else:
-            print("Uneseno:",x, "nije moguce enkriptirati, pokusajte ponovno.")
+    if zelimrazmak == 0 or zelimrazmak == 1:
+        kljuc = int(input("Unesite prvi broj kljuca: ")), int(input("Unesite drugi broj kljuca: "))
+        for x in rijec:
+            if x.isspace() and zelimrazmak == 1:
+                print("",end="")
+            elif x.isspace() and zelimrazmak == 0:
+                print(" ",end="")
+            elif x in abeceda:
+                print(abeceda[(kljuc[0]*int(abeceda.index(x))+kljuc[1]) % 26], end="")
+            else:
+                print("Uneseno:",x, "nije moguce enkriptirati, pokusajte ponovno.")
+    else:
+        print(zelimrazmak, "nije niti 0 niti 1, idiote jedan.")
 
 elif ekripIliDekrip is 1:
     print("dekr") #Wip
