cc2f2205206ce2296521835efa080dce6fd6e3da
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2019-03-05 18:47:27 +0100

    edit

--- AfinaSifra.py
+++ AfinaSifra.py
@@ -2,18 +2,18 @@ abeceda = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","
 ekripIliDekrip = int(input("Enkriptiranje(0) ili dekriptiranje(1)? "))
 if ekripIliDekrip is 0:
     rijec = input("Unesi rijec koju zelis enkriptirati: ").lower()
-    zelimrazmak = input("Zelis li sakriti razmake, Da = 1, Ne = 0?: ")
-    kljuc = int(input("Unesi prvi broj kljuca: ")),int(input("Unesi drugi broj kljuca: "))
+    zelimrazmak = int(input("Zelite li sakriti razmake, Da = 1, Ne = 0?: "))
+    kljuc = int(input("Unesite prvi broj kljuca: ")),int(input("Unesite drugi broj kljuca: "))
     kriplistaslova = []
     for x in rijec:
-        if x == " " and zelimrazmak == 1:
-            print("")
-        elif x == " " and zelimrazmak == 0:
-            print(" ")
+        if x.isspace() and zelimrazmak == 1:
+            print("",end="")
+        elif x.isspace() and zelimrazmak == 0:
+            print(" ",end="")
         elif x in abeceda:
             print(abeceda[(kljuc[0]*int(abeceda.index(x))+kljuc[1]) % 26], end="")
         else:
-            print("Uneseno ",x, "nije moguce enkriptirai, pokusaj ponovno.")
+            print("Uneseno:",x, "nije moguce enkriptirati, pokusajte ponovno.")
 
 elif ekripIliDekrip is 1:
     print("dekr") #Wip
--- RandomTestiranje.py
+++ RandomTestiranje.py
@@ -1,1 +1,10 @@
-tqcwtqckiqrwnoqobcewoqvkbukapkoqoqbcqpqajgudqeqorwtsjgrweqkywgtwcjkrbikzgvogbq
+# tqcwtqckiqrwnoqobcewoqvkbukapkoqoqbcqpqajgudqeqorwtsjgrweqkywgtwcjkrbikzgvogbq
+rijec = input().lower()
+y=1
+x = str
+for x in rijec:
+    y=y+1
+    if x.isspace() and True:
+        print("Radi",y)
+    else:
+        print(x)
