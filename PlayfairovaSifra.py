090ee69199f41a6651c969204007e12dd85ec289
Ivan Peranovic <crimson6999@gmail.com>
2019-03-06 22:57:47 +0100

    edit

--- PlayfairovaSifra.py
+++ PlayfairovaSifra.py
@@ -1,2 +1,33 @@
 abeceda = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-print(abeceda)
+matrica = [[0 for x in range(5)] for y in range(5)]
+listaSlovauRijeci = []
+abecedaBezSlova = []
+rijec = input("Unesite rijec koju zelite enkriptirati: ").lower()
+g=0
+h=0
+j=0
+k=0
+
+
+
+for y in abeceda:
+    if y in rijec:
+        listaSlovauRijeci.append(y)
+for c in listaSlovauRijeci:
+    if g != 5 :
+        matrica[h][g] = c
+        g = g+1
+    elif g == 5:
+        g=0
+        h=h+1
+        matrica[h][g] = c
+for v in abeceda:
+    if v not in matrica:
+        if j != 5:
+            matrica[k][j] = v
+            j=j+1
+        elif j == 5:
+            j = 0
+            k = k+1
+            matrica[k][j] = v
+print(matrica)
--- RandomTestiranje.py
+++ RandomTestiranje.py
@@ -1,10 +1,3 @@
 # tqcwtqckiqrwnoqobcewoqvkbukapkoqoqbcqpqajgudqeqorwtsjgrweqkywgtwcjkrbikzgvogbq
-rijec = input().lower()
-y=1
-x = str
-for x in rijec:
-    y=y+1
-    if x.isspace() and True:
-        print("Radi",y)
-    else:
-        print(x)
+lista = [17 for x in range(5)]
+print(lista)
