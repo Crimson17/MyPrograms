cf23336dc500abb42ebd245eef8ed0a4ce272973
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2019-05-12 12:53:23 +0200

    edit

--- PythonToExeMaker.py
+++ PythonToExeMaker.py
@@ -1,25 +1,58 @@
-import os
+import easygui
+from tkinter import *
+
+ikone = False
+ico = False
+
+def ikonFunc():
+    global ikone
+    ikone = True
+    window.destroy()
 
 
-path = r"C:\Users\Ivan\Documents\GitHub\MyPrograms"
+def imamIco():
+    global ico
+    ico = True
+    window2.destroy()
 
-blokada = 0
+#odabir = easygui.fileopenbox("Odaberi file: ","",r"C:\Users\Ivan\Documents\GitHub\MyPrograms\DefaultOpener")
+#dest = easygui.diropenbox("Odaberi destinaciju: ","",r"C:\Users\Ivan\Desktop")
 
-listaFajlova = []
 
-for r, d, f in os.walk(path):
-    for file in f:
-        if file.endswith(".py"):
-            listaFajlova.append(file)
 
-for c in listaFajlova:
-    print(listaFajlova.index(c), c)
+window = Tk()
+window.title("Ikone")
+window.resizable(width=False, height=False)
+window.geometry("800x600")
+photo1 = PhotoImage(file="imaga.gif")
+daButton = PhotoImage(file="da.gif")
+neButton = PhotoImage(file="ne.gif")
+#tekstSlika = PhotoImage(file="tekst.gif")
+Label(window, image=photo1, bg="white") .place(x=-2, y=-2)
+Label(window, text="Zelis li dodati svoju ikonicu?", fg="white", bg="black", font="none 19 bold") .place(relx=0.5, rely=0.27,anchor=CENTER)
+Button(window, image=daButton, command=ikonFunc) .place(relx=0.333, rely=0.55, anchor=CENTER)
+Button(window, image=neButton, command=window.destroy) .place(relx=0.666, rely=0.55, anchor=CENTER)
+window.mainloop()
 
-odabir = int(input("Odaberi file(Broj ispred): "))
+if ikone == True:
+    window2 = Tk()
+    window2.title("IcoFile-ovi")
+    window2.resizable(width=False, height=False)
+    window2.geometry("800x600")
+    photo1 = PhotoImage(file="imaga.gif")
+    daButton = PhotoImage(file="da.gif")
+    neButton = PhotoImage(file="ne.gif")
+    Label(window2, image=photo1, bg="white") .place(x=-2, y=-2)
+    Label(window2, text="Imas li vec IcoFile(Da)? ili zelis pretvoriti nesta drugo u Ico(Ne)?", bg="black", fg="white", font="none 16 bold") .place(relx=0.5, rely=0.27, anchor=CENTER)
+    Button(window2, image=daButton, command=imamIco) .place(relx=0.333, rely=0.55, anchor=CENTER)
+    Button(window2, image=neButton, command=window2.destroy) .place(relx=0.666, rely=0.55, anchor=CENTER)
+    window2.mainloop()
 
-if odabir < 0 or odabir > len(listaFajlova)-1:
-    print("Upisao si krivi broj idiote..")
-    blokada = 1
+    if ico == True:
+        odabirIcoa = easygui.fileopenbox("Odaberi Ico file: ", "", r"C:\Users\Ivan\Documents\GitHub\MyPrograms\DefaultOpener")
+    else:
+        odabirImagea = easygui.fileopenbox("Odaberi Image file: ", "", r"C:\Users\Ivan\Documents\GitHub\MyPrograms\DefaultOpener")
 
-if blokada == 0:
-    destinacija = r(input("Unesi destinacju exe-a: "))
+else:
+    print("WIP")
+
