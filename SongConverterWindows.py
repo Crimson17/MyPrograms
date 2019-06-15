e8df98b47ed670960c0396171e1116b9bcaf7aab
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2019-06-15 13:47:37 +0200

    edit

---
+++ SongConverterWindows.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+
+linkInput = str(input('Unesite Youtube link: '))
+odbariFormata = int(input('Samo zvuk?(0) ili video?(1) '))
+
+if odbariFormata == 1:
+    print('wip')
+else:
+    browser = webdriver.Chrome(r'C:\Users\Ivan\Documents\WebDriver\chromedriver.exe')
+    browser.get('https://www.onlinevideoconverter.com/mp3-converter')
+    browser.find_element_by_id('texturl').send_keys(linkInput)
+    browser.find_element_by_id('convert1').click()
+
+
+
