2cc06e7b79c117617beb7ce44e040b36c5ec2fea
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2019-06-16 20:25:57 +0200

    edit

--- SongConverterWindows.py
+++ SongConverterWindows.py
@@ -1,16 +1,43 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time, os
 
 
+y = 0
+listOfFiles = []
 linkInput = str(input('Unesite Youtube link: '))
-odbariFormata = int(input('Samo zvuk?(0) ili video?(1) '))
+folderPath = r"C:\Users\Ivan\Desktop\YT_Download"
 
-if odbariFormata == 1:
-    print('wip')
-else:
-    browser = webdriver.Chrome(r'C:\Users\Ivan\Documents\WebDriver\chromedriver.exe')
-    browser.get('https://www.onlinevideoconverter.com/mp3-converter')
-    browser.find_element_by_id('texturl').send_keys(linkInput)
-    browser.find_element_by_id('convert1').click()
+if not os.path.isdir(folderPath):
+    os.mkdir(folderPath)
 
+options = webdriver.ChromeOptions()
+preferences = {"download.default_directory": folderPath, "safebrowsing.enabled": "false"}
 
+options.add_experimental_option("prefs", preferences)
 
+browser = webdriver.Chrome(r"C:\Users\Ivan\Documents\WebDriver\chromedriver.exe", options=options)
+browser.get('https://ytmp3.cc/')
+browser.find_element_by_id('input').send_keys(linkInput)
+browser.find_element_by_id('submit').click()
+
+try:
+    while browser.find_element_by_xpath('//*[@id="download" and @href=""]'):
+        print('Please wait.')
+        time.sleep(1)
+except NoSuchElementException:
+    browser.find_element_by_id('download').click()
+
+time.sleep(1.5)
+
+while y == 0:
+    for r, d, f in os.walk(folderPath):
+        for file in f:
+            listOfFiles.append(os.path.join(r, file))
+    if any("crdownload" in s for s in listOfFiles):
+        time.sleep(0.5)
+        listOfFiles = []
+    else:
+        time.sleep(1)
+        y = 1
+        browser.quit()
