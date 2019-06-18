6f097850a74506aa2829a4e0d6158c6390e1559d
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2019-06-18 22:58:47 +0200

    edit

--- PhoneToPc.py
+++ PhoneToPc.py
@@ -2,30 +2,36 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time, os, easygui
 
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications" : 2}
-chrome_options.add_experimental_option("prefs",prefs)
-chrome_options.add_argument("--start-maximized")
-browser = webdriver.Chrome(r"C:\Users\Ivan\Documents\GitHub\MyPrograms\ChromeVersions\75.exe", options=chrome_options)
+#chrome_options = webdriver.ChromeOptions()
+#prefs = {"profile.default_content_setting_values.notifications" : 2}
+#chrome_options.add_experimental_option("prefs",prefs)
+#chrome_options.add_argument("--start-maximized")
+#browser = webdriver.Chrome(r"C:\Users\Ivan\Documents\GitHub\MyPrograms\ChromeVersions\75.exe", options=chrome_options)
 #browser.get(r"https://www.facebook.com/")
-browser.get(r"https://www.facebook.com/messages/t/crimson.ivan.9")
+#browser.get(r"https://www.facebook.com/messages/t/crimson.ivan.9")
 
-browser.find_element_by_id("email").send_keys("crimson6999@gmail.com")
-time.sleep(0.2)
-browser.find_element_by_id("pass").send_keys("justtesting")
-time.sleep(0.2)
-browser.find_element_by_id("loginbutton").click()
-time.sleep(0.3)
+#browser.find_element_by_id("email").send_keys("crimson6999@gmail.com")
+#time.sleep(0.2)
+#browser.find_element_by_id("pass").send_keys("justtesting")
+#time.sleep(0.2)
+#browser.find_element_by_id("loginbutton").click()
+#time.sleep(0.3)
 
-message = browser.find_element_by_xpath('//*[@id="row_header_id_user:100022865750608"]/a/div/div[2]/div[2]/span/span').text
+#message = browser.find_element_by_xpath('//*[@id="row_header_id_user:100022865750608"]/a/div/div[2]/div[2]/span/span').text
 
-while message != "stop":
-    message = browser.find_element_by_xpath('//*[@id="row_header_id_user:100022865750608"]/a/div/div[2]/div[2]/span/span').text
-    print(message)
-    time.sleep(1.5)
+#while message != "stop":
+#    message = browser.find_element_by_xpath('//*[@id="row_header_id_user:100022865750608"]/a/div/div[2]/div[2]/span/span').text
+#    print(message)
+#    time.sleep(1.5)
 
 #browser.find_element_by_xpath('//*[@id="js_12"]/div').click()
 #time.sleep(10)
 #browser.quit()
 
+message = open(r"C:\Users\Ivan\Dropbox\AutoSongPlayer\Song.txt").read()
 
+while "1709" not in message:
+    time.sleep(1.5)
+    message = open(r"C:\Users\Ivan\Dropbox\AutoSongPlayer\Song.txt").read()
+    print(message)
+print("Stopped.")
