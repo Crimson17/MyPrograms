31f54078b84d57b98780cdcf360953013ed85255
Ivan Peranovic <crimson6999@gmail.com>
2020-11-04 20:32:08 +0100

    Update app.py

--- MySite/app.py
+++ MySite/app.py
@@ -2,7 +2,9 @@ from flask import Flask
 import socket
 
 myIP = socket.gethostbyname(socket.gethostname())
+print(myIP)
 
+"""
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,3 +13,4 @@ def main():
 
 if __name__ == "__main__":
     app.run(debug=True, host=myIP, port=80)
+"""
